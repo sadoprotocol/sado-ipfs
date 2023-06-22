8da4171bd7b3544493539f09746843edf05f204a
Nishant Ghodke <nishant.ghodke@cakedefi.com>
2023-06-22 18:28:00 +0530

    feat: add global error handler

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { type NextFunction, type Request, type Response, Router } from "express";
 
 import IPFSRouter from "./ipfs";
 
@@ -8,6 +8,14 @@ const routers = [IPFSRouter];
 // Dynamically bind routers to the main application router
 routers.forEach((router) => {
 	applicationRouter.use(router.basePath, router.router);
+});
+
+// ❗Important note: This error handler needs to be the very last route in the application router stack
+applicationRouter.use((error: Error, _request: Request, response: Response, _next: NextFunction) => {
+	return response.json({
+		status: false,
+		error: error.message,
+	});
 });
 
 export default applicationRouter;
