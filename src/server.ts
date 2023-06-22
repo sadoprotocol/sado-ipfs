afa1f1e03feb2345f485fceff38fd11c870457f1
Nishant Ghodke <nishant.ghodke@cakedefi.com>
2023-06-22 23:21:57 +0530

    chore: add build, lint, and start script
    also, downgrade typescript to 5.0.4 to make eslint work

--- src/server.ts
+++ src/server.ts
@@ -11,5 +11,5 @@ app.use(express.json({ limit: MAXIMUM_JSON_PAYLOAD_SIZE }));
 app.use(applicationRouter);
 
 app.listen(process.env.PORT, () => {
-	console.log(`IPFS API is up & running at http://localhost:${process.env.PORT}`);
+	console.log(`Sado IPFS is running at http://localhost:${process.env.PORT} | env: ${process.env.NODE_ENV}`);
 });
